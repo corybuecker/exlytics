37898f8d7c57fa18f458264a9e3d28ff1df801fa
Cory Buecker <cory.buecker@gmail.com>
2020-04-21 19:58:53 -0500

    Update rollups

--- priv/repo/migrations/20200422011028_update_materialized_views.exs
+++ priv/repo/migrations/20200422011028_update_materialized_views.exs
@@ -1,7 +1,51 @@
-defmodule Exlytics.Repo.Migrations.CreateMaterializedViews do
+defmodule Exlytics.Repo.Migrations.UpdateMaterializedViews do
   use Ecto.Migration
 
   def up do
+    execute("drop view if exists page_views cascade;")
+    execute("drop view if exists specific_page_views cascade;")
+    execute("drop view if exists link_clicks cascade;")
+    execute("drop view if exists specific_link_clicks cascade;")
+
+    execute("""
+      CREATE VIEW page_views WITH (timescaledb.continuous) AS
+      SELECT
+        time_bucket ('1day', time) AS day,
+        metadata ->> 'host' as host,
+        metadata ->> 'page' as page,
+        count(1)
+      FROM
+        events
+      GROUP BY
+        day,
+        host,
+        page
+      ;
+    """)
+
+    execute("""
+      CREATE VIEW link_clicks WITH (timescaledb.continuous) AS
+      SELECT
+        time_bucket ('1day', time) AS day,
+        metadata ->> 'host' as host,
+        metadata ->> 'click_link' as link,
+        count(1)
+      FROM
+        events
+      GROUP BY
+        day,
+        host,
+        link
+      ;
+    """)
+  end
+
+  def down do
+    execute("drop view if exists page_views cascade;")
+    execute("drop view if exists specific_page_views cascade;")
+    execute("drop view if exists link_clicks cascade;")
+    execute("drop view if exists specific_link_clicks cascade;")
+
     execute("""
       CREATE VIEW page_views WITH (timescaledb.continuous) AS
       SELECT
@@ -61,12 +105,5 @@ defmodule Exlytics.Repo.Migrations.CreateMaterializedViews do
         click_link
       ;
     """)
-  end
-
-  def down do
-    execute("drop view if exists page_views cascade;")
-    execute("drop view if exists specific_page_views cascade;")
-    execute("drop view if exists link_clicks cascade;")
-    execute("drop view if exists specific_link_clicks cascade;")
   end
 end

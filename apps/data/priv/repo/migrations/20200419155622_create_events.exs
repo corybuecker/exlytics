ebb8ebebf63b125a9931722d63aa6baab42572a4
Cory Buecker <cory.buecker@gmail.com>
2020-06-07 14:26:22 -0500

    Create dependabot.yml

--- apps/data/priv/repo/migrations/20200419155622_create_events.exs
+++ apps/data/priv/repo/migrations/20200419155622_create_events.exs
@@ -2,15 +2,11 @@ defmodule Exlytics.Data.Repo.Migrations.CreateEvents do
   use Ecto.Migration
 
   def change do
-    execute("create extension if not exists timescaledb", "")
-
     create table(:events, [{:primary_key, false}]) do
       add(:time, :utc_datetime_usec, null: false)
       add(:metadata, :map)
       timestamps()
     end
-
-    execute("select create_hypertable('events', 'time')", "")
 
     create(index(:events, ["time", "metadata"]))
     create(index(:events, ["metadata"], using: :gin))
--- apps/data/priv/seeds.exs
+++ apps/data/priv/seeds.exs
@@ -11,6 +11,8 @@ pages = [
   nil
 ]
 
+%Exlytics.Data.Account{id: account_id} = Exlytics.Data.Account |> Ecto.Query.first() |> Exlytics.Data.Repo.one()
+
 Enum.each(0..2000, fn _ ->
   {:ok, datetime} =
     with {:ok, date} <- Date.new(Enum.random(2018..2020), Enum.random(1..12), Enum.random(1..28)),
@@ -21,6 +23,7 @@ Enum.each(0..2000, fn _ ->
 
   %Exlytics.Data.Event{
     time: datetime,
+    account_id: account_id,
     metadata: %{
       host: "exlytics.corybuecker.com",
       user_agent: user_agents |> Enum.random(),

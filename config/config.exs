7ec59325cf9a0a2ca305c96ad7df588dbbc047f2
Cory Buecker <cory.buecker@gmail.com>
2020-06-21 07:05:24 -0500

    Back to Cloud run

--- config/config.exs
+++ config/config.exs
@@ -5,5 +5,6 @@ config :exlytics,
   port: System.get_env("PORT"),
   ecto_repos: [Exlytics.Data.Repo]
 
-config :exlytics, Exlytics.Data.Repo, url: System.get_env("DATABASE_CONNECTION")
+config :exlytics, Exlytics.Data.Repo, url: System.get_env("DATABASE_CONNECTION"), pool_size: 3
+
 import_config "#{Mix.env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,1 @@
 import Config
-
-config :exlytics,
-  host: "localhost",
-  port: "8080"
-
-config :exlytics, Exlytics.Data.Repo, url: "ecto://exlytics@localhost:5432/exlytics?ssl=false"

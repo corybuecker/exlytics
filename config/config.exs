ba04cfb13a8ba4eef64eaced848187dc9ebaa925
Cory Buecker <cory.buecker@gmail.com>
2020-06-23 08:49:35 -0500

    Add tests for routers

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,8 @@
 import Config
 
-config :exlytics,
-  host: System.get_env("HOST"),
-  port: System.get_env("PORT"),
-  ecto_repos: [Exlytics.Data.Repo]
+config :exlytics, host: "override", port: "override"
 
-config :exlytics, Exlytics.Data.Repo, url: System.get_env("DATABASE_CONNECTION"), pool_size: 3
+config :exlytics, ecto_repos: [Exlytics.Data.Repo]
+config :exlytics, Exlytics.Data.Repo, pool_size: 3
 
 import_config "#{Mix.env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,7 @@
 import Config
 
-config :exlytics, host: "localhost", port: "4000", ecto_repos: [Exlytics.Data.Repo]
+config :exlytics, host: "localhost", port: "4000"
 
 config :exlytics, Exlytics.Data.Repo,
-  url: System.get_env("DATABASE_CONNECTION_URL", "ecto://postgres@localhost:5432/exytics_test")
+  url: System.get_env("DATABASE_CONNECTION_URL", "ecto://postgres@localhost:5432/exytics_test"),
+  pool: Ecto.Adapters.SQL.Sandbox

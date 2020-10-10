15a72791a55d6dc7264716d8f89be784f080fa79
Cory Buecker <cory.buecker@gmail.com>
2020-10-10 09:33:52 -0500

    Upgrade to Elixir 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exlytics.MixProject do
     [
       app: :exlytics,
       version: "1.0.0",
-      elixir: "1.10.4",
+      elixir: "1.11.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,12 +23,12 @@ defmodule Exlytics.MixProject do
   defp deps do
     [
       {:plug_cowboy, "~> 2.3.0"},
-      {:credo, "~> 1.4.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4.1", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false},
       {:lettuce, "~> 0.1.0", only: [:dev]},
-      {:ecto_sql, "~> 3.4.5"},
+      {:ecto_sql, "~> 3.5.0"},
       {:postgrex, ">= 0.0.0"},
-      {:jason, "~> 1.2.0"}
+      {:jason, "~> 1.2.2"}
     ]
   end
 end

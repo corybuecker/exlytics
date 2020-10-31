6a445f53539fe6e9d4b48153626aaba94b8862a2
Cory Buecker <email@corybuecker.com>
2020-10-31 09:39:23 -0500

    fix test URL

--- config/test.exs
+++ config/test.exs
@@ -3,10 +3,7 @@ import Config
 config :exlytics, host: "localhost", port: "4000"
 
 config :exlytics, Exlytics.Data.Repo,
-  username: "postgres",
-  password: "",
-  database: "exlytics_test",
-  migration_default_prefix: "public",
-  hostname: "localhost",
+  url: "ecto://postgres:@localhost:5432/exlytics_test",
+  migration_default_prefix: "exlytics",
   show_sensitive_data_on_connection_error: true,
   pool: Ecto.Adapters.SQL.Sandbox

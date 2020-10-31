ee5e906c39edc9aff2c40373712aba0de1bbe352
Cory Buecker <email@corybuecker.com>
2020-10-31 11:14:05 -0500

    use sockets

--- config/releases.exs
+++ config/releases.exs
@@ -3,5 +3,5 @@ import Config
 config :exlytics, host: System.get_env("HOST"), port: System.get_env("PORT")
 
 config :exlytics, Exlytics.Data.Repo,
-  url: System.get_env("DATABASE_CONNECTION"),
+  socket: System.get_env("DATABASE_CONNECTION"),
   migration_default_prefix: "exlytics"

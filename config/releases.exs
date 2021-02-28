a6dbb3be33ee5b3752c86af54a342b831afff65d
Cory Buecker <email@corybuecker.com>
2021-02-28 12:32:26 -0600

    Use URL

--- config/releases.exs
+++ config/releases.exs
@@ -3,7 +3,5 @@ import Config
 config :exlytics, host: System.get_env("HOST"), port: System.get_env("PORT")
 
 config :exlytics, Exlytics.Data.Repo,
-  username: "exlytics",
-  password: "exlytics",
-  database: "exlytics",
+  url: System.get_env("DATABASE_URL"),
   migration_default_prefix: "exlytics"

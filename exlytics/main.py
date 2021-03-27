2013dd6d866797d882e1e988eb8a11d9fd62d37a
Cory Buecker <email@corybuecker.com>
2021-03-27 12:51:10 -0500

    Add healthcheck

--- exlytics/main.py
+++ exlytics/main.py
@@ -1,9 +1,17 @@
-from sanic import Sanic
 from exlytics.utils import setup_db
 from exlytics.view import ExlyticsView
+from sanic import Sanic
+from sanic.response import empty
 
 app = Sanic('exlytics')
 app.register_listener(setup_db, "before_server_start")
 
 app.add_route(ExlyticsView.as_view(), "/")
+
+
+@app.get("/healthcheck")
+async def healthcheck(request):
+    return empty()
+
+
 app.run(host='0.0.0.0', port=5000, debug=True)

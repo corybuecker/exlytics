0aeae824febba6ce8d3fdb6e3c9826332651aad5
Cory Buecker <email@corybuecker.com>
2022-04-17 12:49:19 -0500

    Add back Quantum support for flushing events

--- config/production.exs
+++ config/production.exs
@@ -1,3 +1,8 @@
 import Config
 
+config :exlytics, Scheduler,
+  jobs: [
+    {"*/30 * * * *", {Exlytics.Storage.GoogleStorage.Loader, :load, []}}
+  ]
+
 config :exlytics, storage: Exlytics.Storage.GoogleStorage, container: "bueckered-exlytics-storage"

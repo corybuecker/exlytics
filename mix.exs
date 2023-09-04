8e23d942c1c8181125351080bf437f4044c47fa3
Cory Buecker <email@corybuecker.com>
2023-09-04 10:15:24 -0500

    Update deps (#90)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exlytics.MixProject do
     [
       app: :exlytics,
       version: "1.0.2",
-      elixir: "1.15.0",
+      elixir: "1.15.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,11 +23,11 @@ defmodule Exlytics.MixProject do
   defp deps do
     [
       {:castore, ">= 0.0.0"},
-      {:certifi, "~> 2.9"},
+      {:certifi, "~> 2.12"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:google_api_storage, "~> 0.34"},
-      {:goth, "~> 1.2"},
+      {:goth, "~> 1.4"},
       {:plug_cowboy, "~> 2.5"},
       {:redix, "~> 1.1"},
       {:uuid, "~> 1.1"}

546e264d3e935ca3ecc87a2cd271d7ac6296d25f
Cory Buecker <email@corybuecker.com>
2023-11-04 09:45:52 -0500

    add README and logging (#91)

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :exlytics, cache: Exlytics.Cache.Redis
 config :exlytics, host: "override", port: "override"
-config :exlytics, redis: "redis://localhost:6379/3"
+config :exlytics, redis: "redis://localhost:6379/0"
 config :exlytics, storage: Exlytics.Storage.GoogleStorage, container: "bueckered-exlytics"
 config :exlytics, time_adapter: Exlytics.Utils.TimeAdapter
 
